edfe5f0f9b45b77c281b731f4760226bdbae1a64
Phyo <soewaiphyo.jelly@gmail.com>
2020-10-17 21:28:42 +0800

    cat component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,57 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Grid } from "@material-ui/core";
+
+import CatContainer from "./cat-component";
+
+class Cat {
+  constructor(name, age, eyeColor, profileUrl) {
+    this.name = name;
+    this.age = age;
+    this.eyeColor = eyeColor;
+    this.profileUrl = profileUrl;
+  }
+}
 
 function App() {
-  function showDate() {
-    console.log(Date());
-  }
-
-  const arrowShowDate = () => console.log(Date());
-  const arrowMyName = (name, age) => (
-    <div>
-      hey ... {name}. I am {age} years old.
-    </div>
+  const catA = new Cat(
+    "Phyu lay",
+    2,
+    "green",
+    "https://static.scientificamerican.com/sciam/cache/file/92E141F8-36E4-4331-BB2EE42AC8674DD3_source.jpg?w=590&h=800&62C6A28D-D2CA-4635-AA7017C94E6DDB72"
+  );
+  const catB = new Cat(
+    "MeeNyoung",
+    7,
+    "yellow",
+    "https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2018/08/kitten-440379.jpg?h=c8d00152&itok=1fdekAh2"
   );
 
-  //Destructure syntax
-  const getProfile = (profile) => {
-    const { name, age, sex } = profile;
+  const catC = new Cat(
+    "MeeNyoung",
+    7,
+    "yellow",
+    "https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2018/08/kitten-440379.jpg?h=c8d00152&itok=1fdekAh2"
+  );
 
-    return (
-      <div>
-        Hello I am {name}. I am {age} years old. I am {sex}.
-      </div>
-    );
-  };
+  const cats = [catA, catB, catC];
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Cat Listing</h1>
+      <div style={{ width: "100%" }}>
+        <div
+          style={{
+            maxWidth: "600px",
+            margin: "0 auto",
+          }}
         >
-          Learn React
-        </a>
-        {getProfile({
-          name: "Lolita",
-          age: 6,
-          sex: "Female",
-        })}
-      </header>
+          {cats.map((cat) => (
+            <CatContainer cat={cat} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
