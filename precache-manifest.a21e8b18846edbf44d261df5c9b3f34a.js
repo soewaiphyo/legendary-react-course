2b402205b84a89c597b8fca2fa15540322bb33c6
Phyo <soewaiphyo.jelly@gmail.com>
2020-10-20 16:29:46 +0800

    Updates

---
+++ precache-manifest.a21e8b18846edbf44d261df5c9b3f34a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa306445d1b8a0ca27498fa9ae44ed8d",
+    "url": "/legendary-react-course/index.html"
+  },
+  {
+    "revision": "693bef21ec6452b5adbd",
+    "url": "/legendary-react-course/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "42934297c87b4f18cfc1",
+    "url": "/legendary-react-course/static/js/2.05d183da.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/legendary-react-course/static/js/2.05d183da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "693bef21ec6452b5adbd",
+    "url": "/legendary-react-course/static/js/main.14e6b82b.chunk.js"
+  },
+  {
+    "revision": "6582361b4eb3be14050a",
+    "url": "/legendary-react-course/static/js/runtime-main.78057ffa.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/legendary-react-course/static/media/logo.ee7cd8ed.svg"
+  }
+]);
